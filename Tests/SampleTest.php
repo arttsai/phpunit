13f9ca5aa1ccd5f908dab994be8cb6e71be89696
Art Tsai <arttsai@gmail.com>
2016-04-19 15:17:27 +0800

    add folder Tests and its test php files

---
+++ Tests/SampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Acme\Tests;
+
+/**
+ *
+ */
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+  public function testIsTrue()
+  {
+    $myvar = true;
+    $this->assertTrue($myvar);
+  }
+
+}
